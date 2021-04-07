c253000b33d59144a9d6991e9eb489310b842f4e
NovaPC <novapc@novapc.com.br>
2021-04-07 17:55:52 -0300

    Remove CNPJ validation when saving configs in "Melhor Envio > Configurações Gerais"

--- NovaPC_Melhorenvio/app/code/community/NovaPC/Melhorenvio/Model/Observer.php
+++ NovaPC_Melhorenvio/app/code/community/NovaPC/Melhorenvio/Model/Observer.php
@@ -9,32 +9,6 @@ class NovaPC_Melhorenvio_Model_Observer
 			$etiquetaExists = Mage::helper('melhorenvio')->isEtiquetaValidFromOrder($order->getId());
 		}
 	}
-
-	public function validateDataSavedInSystemXml(Varien_Event_Observer $event)
-	{
-		$section = Mage::app()->getRequest()->getParams();
-		if($section['section'] === 'melhorenvio') {
-			$empresaSystem = $section['groups']['empresa']['fields'];
-
-			$dataMelhorEnvio = Mage::helper('melhorenvio')->getUserDataFromMelhorEnvio();
-
-			$cpfMagento = $empresaSystem['cpf']['value'];
-			$cpfMelhorEnvio = $dataMelhorEnvio->document;
-			$cnpjMagento = $empresaSystem['cnpj']['value'];
-			$cnpjMelhorEnvio = $dataMelhorEnvio->company_document;
-
-			$session = Mage::getSingleton('adminhtml/session');
-			
-			if($this->cleanCpfCnpj($cnpjMagento) != $cnpjMelhorEnvio) {
-				$session->addWarning('O CNPJ está diferente do cadastrado na Melhor Envio.');
-			}
-		}
-	}
-
-	protected function cleanCpfCnpj($cpfcnpj)
-	{
-		return preg_replace('/[.\-,\/]/', '', $cpfcnpj);
-	}
 }
 
 ?>
